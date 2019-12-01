e21255fc82c0fc043dd70008b77859ad29180bb7
intervisionlord <ads-net@mail.ru>
2019-12-02 01:45:25 +0300

    0.1.4 DEV
     - Readme
     - Code of conduct
     - Правка копирайтов
     - Правка заголовка
     - Более глубокая проверка локалей
     - Правка системных переменных

--- core/core.php
+++ core/core.php
@@ -1,4 +1,10 @@
 <?php
+/**
+* Базовые алгоритмы
+*
+* @author Intervision (https://github.com/intervisionlord)
+* Copyright © 2019 Intervision
+*/
 require_once('./conf/system.conf.php');
 require_once('./core/functions.php');
 
@@ -8,7 +14,11 @@ if ($FORCE_LOCALE == '0') {
   $CUR_LOCALE = $FORCE_LOCALE;
 }
 
-require_once('L10n/'.$CUR_LOCALE.'/lang.php');
+if (!file_exists('./L10n/'.$CUR_LOCALE.'/lang.php') && $FORCE_LOCALE == '0') {
+  $CUR_LOCALE = 'ru_RU';
+} else {
+  require_once('./L10n/'.$CUR_LOCALE.'/lang.php');
+}
 
 if (!file_exists('./conf/auth.conf_secret.php')) {
   require_once('./conf/auth.conf.php');
